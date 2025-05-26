43b01cbdce446aa024ae2f588d520829dd53106d
Ved Khandalkar <72626586+ElmoFromSpace@users.noreply.github.com>
2025-05-26 15:27:32 -0400

    Delete May26Signup directory

--- May26Signup/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import React, { useState } from 'react';
-import { SignupPage } from './components/SignupPage';
-export function App() {
-  return <div className="flex w-full min-h-screen bg-[#e7e4d4] justify-center items-center p-4">
-      <SignupPage />
-    </div>;
-}
